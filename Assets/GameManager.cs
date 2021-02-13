2bca01e8f0e749386b45be903e741bd80216f34b
ys <leyosu22@gmail.com>
2021-02-12 19:40:56 -0500

    Initial Commit
    Initial Commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+
+    // Start is called before the first frame update
+    public void EndGame()
+    {
+        if (!gameEnded)
+        {
+            gameEnded = true;
+            Debug.Log("game over");
+            FindObjectOfType<AudioManager>().Play("fail");
+            FindObjectOfType<AudioManager>().Stop("Theme");
+
+
+            //Invoke("Restart", 5.0f);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        FindObjectOfType<AudioManager>().Play("Theme");
+    }
+}
